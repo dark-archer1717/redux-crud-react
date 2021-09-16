5adc652936b103acb112df5aafe08e9259534c28
Efazul Fayad <efazul.fayad1717@gmail.com>
2021-09-17 01:26:26 +0600

    commit

---
+++ src/FakeData.js
@@ -0,0 +1,42 @@
+export const UsersData = [
+  {
+    id: 1,
+    name: "Christian Bell",
+    username: "Bell",
+  },
+  {
+    id: 2,
+    name: "Robert Pattinson",
+    username: "Pattinson",
+  },
+  {
+    id: 3,
+    name: "Anne Hathaway",
+    username: "Anne",
+  },
+  {
+    id: 4,
+    name: "Patricia Lebsack",
+    username: "Karianne",
+  },
+  {
+    id: 5,
+    name: "Marlon Brando",
+    username: "Marlonmren",
+  },
+  {
+    id: 6,
+    name: "Jake Runolfsdottir V",
+    username: "Leopoldo_Corkery",
+  },
+  {
+    id: 7,
+    name: "Mrs. Dennis Schulist",
+    username: "Karley_Dach",
+  },
+  {
+    id: 8,
+    name: "Kurtis Weissnat",
+    username: "Elwyn.Skiles",
+  },
+];
